c0a69ebe2c49d7340bf73ca7c6afc515b071c2a3
Francyne Rigor <111488342+cynrgr@users.noreply.github.com>
2023-05-14 20:54:08 +0800

    Create Tao.java

---
+++ Machine Problems/oop/Tao.java
@@ -0,0 +1,24 @@
+package oop;
+
+public class Tao {
+	
+	private String pangalan = "";
+	private int edad = 0;
+	
+	public int getEdad() {
+		return edad;
+	}
+	
+	public String getPangalan() {
+		return pangalan;
+	}
+	
+	public Tao(String pangalan, int edad) {
+			this.pangalan = pangalan;
+			this.edad = edad;
+	}
+
+	
+	}
+	
+
