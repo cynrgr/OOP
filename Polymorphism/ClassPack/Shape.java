cd0cfcbb15d2a7bd40d768119c8b311ea34e93ce
Francyne Rigor <111488342+cynrgr@users.noreply.github.com>
2023-05-15 20:14:35 +0800

    Create Shape.java

---
+++ Polymorphism/ClassPack/Shape.java
@@ -0,0 +1,21 @@
+package ClassPack;
+
+public class Shape {
+	private double height;
+	private double width;
+	
+	public double getHeight() {
+		return height;
+	}
+
+	public double getWidth() {
+		return width;
+	}
+	
+	public Shape (double height, double width){
+		this.height = height;
+		this.width = width;
+	}
+	
+
+}
